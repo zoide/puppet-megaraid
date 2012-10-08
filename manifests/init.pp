53fe420a471d4ec9871d315629d234896545bb51
Andy Shinn <andys@andyshinn.as>
2012-10-08 16:26:04 -0700

    initial init commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,40 +2,24 @@
 #
 # Full description of class megaraid here.
 #
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
 # === Examples
 #
-#  class { megaraid:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
+#  include megaraid
+#
+#  To include only Storage Manager or MegaCLI call the subclass:
+#
+#  include megaraid::megacli
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Andy Shinn <andy@eye.fi>
 #
 # === Copyright
 #
-# Copyright 2011 Your name here, unless otherwise noted.
+# Copyright 2012 Andy Shinn
 #
 class megaraid {
-
+  include megaraid::megacli
+  include megaraid::storagemanager
 
 }
