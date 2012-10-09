3332fc32195dbeebbfa9022a6d4a2aa007b8d3d1
Andy Shinn <andys@andyshinn.as>
2012-10-08 17:58:24 -0700

    megacli package name update

--- manifests/megacli.pp
+++ manifests/megacli.pp
@@ -1,6 +1,6 @@
 # Install the MegaCLI command line utility
 class megaraid::megacli {
-  $package_name = 'MegaCLI'
+  $package_name = 'MegaCli'
 
   package { $package_name:
     ensure => 'installed',
