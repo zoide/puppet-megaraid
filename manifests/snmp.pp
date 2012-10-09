12e275d4ceb5979b87ab2982d3fe83519b156f4c
Andy Shinn <andys@andyshinn.as>
2012-10-08 17:09:23 -0700

    include sas_snmp and sas_ir_snmp when net-snmp installed

---
+++ manifests/snmp.pp
@@ -0,0 +1,9 @@
+class megaraid::snmp {
+  $sas_packages = ['sas_ir_snmp', 'sas_snmp']
+
+  if (defined(Package['net-snmp'])) {
+    package { $sas_packages:
+      ensure => 'installed',
+    }
+  }
+}
