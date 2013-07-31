3c135eabddf5df2d3c16553d4f2bfab5c8f1aafb
Udo Waechter <u.waechter@mysportgroup.de>
2013-07-31 16:13:04 +0200

    adding the repo-key

--- manifests/debian/repository.pp
+++ manifests/debian/repository.pp
@@ -2,6 +2,7 @@ class megaraid::debian::repository ($ensure = 'present') {
   apt::source { 'megaraid':
     ensure      => $ensure,
     location    => 'http://hwraid.le-vert.net/debian',
+    key_source  => 'http://hwraid.le-vert.net/debian/hwraid.le-vert.net.gpg.key',
     include_src => false,
     release     => 'squeeze',
   }
